aff4fbcdc9dbbeb70c8ce6f1b231999394f223de
unknown <btukenalieva@gmail.com>
2021-01-21 17:40:38 +0600

    added todo list to test page

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import todo
 # Create your views here.
 
 def homepage(request):
     return render(request, "index.html")
 
 def test(request):
-    return render(request, "test.html")
+    todo_list = todo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
 
 def third(request):
     return HttpResponse("This is page test3")
